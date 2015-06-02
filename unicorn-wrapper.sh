fb260b6f5e6cfcc1313198226678b1592017dea4
Stephen Garlick <sgarlick@taximagic.com>
2015-06-02 13:21:58 -0400

    use consul hostname

--- unicorn-wrapper.sh
+++ unicorn-wrapper.sh
@@ -3,5 +3,5 @@ sed -i "s:SERVICE_NAME:$HOSTNAME:g" /etc/confd/conf.d/unicorn.toml
 
 /usr/bin/confd -onetime -backend consul -node consul:8500 \
 	-debug -verbose -config-file /etc/confd/conf.d/unicorn.toml
-export RAILS_ENV=$(curl http://consul.gocurb.internal/v1/kv/cf/config/environment?raw)
+export RAILS_ENV=$(curl http://consul:8500/v1/kv/cf/config/environment?raw)
 exec bundle exec unicorn -c /etc/unicorn/unicorn.rb
